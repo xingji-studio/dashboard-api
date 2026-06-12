9446422ce07f4ab053f6218dcc2198e92b84b1f2
patrick <patrick25yao@outlook.com>
2026-06-12 19:16:25 +0800

    [fix] fku .env (again #1)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
+const envContent = Deno.readTextFileSync(".env");
+const env = Object.fromEntries(
+    envContent.split("\n").filter((l) => l && !l.startsWith("#")).map((l) => l.split("=")),
+);
+
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schema.ts",
     dialect: "sqlite",
     dbCredentials: {
-        url: Deno.env.get("DB_PATH") || "file:./db.sqlite",
+        url: env.DB_PATH || "file:./db.sqlite",
     },
 });
